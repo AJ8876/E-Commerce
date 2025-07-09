b5b9392871c6cb2b8fe8297ce291d26361d96170
Aj8876 <madeelahmad891@gmail.com>
2025-07-09 11:21:42 -0700

    Shopify

--- lib/Dashboard_Screens/Payment_Screen.dart
+++ lib/Dashboard_Screens/Payment_Screen.dart
@@ -130,32 +130,147 @@ class _PaymentScreenState extends State<PaymentScreen> {
                 ],
               ),
             ),
-               Text("Select Payment Method",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),
-               ),
-              SizedBox(height: 14),
-              GestureDetector(
-                onTap: ()=> setState(() => SelectMethod='JazzCash'),
-                child: Container(
-                  margin: EdgeInsets.only(bottom: 10),
-                  padding: EdgeInsets.symmetric(horizontal: 14,vertical: 12),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: SelectMethod== 'JazzCash' ? Colors.deepPurple: Colors.black26),
-                    borderRadius: BorderRadius.circular(12),
-                    color: Colors.white,
+            Text(
+              "Select Payment Method",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 14),
+            GestureDetector(
+              onTap: () => setState(() => SelectMethod = 'JazzCash'),
+              child: Container(
+                margin: EdgeInsets.only(bottom: 10),
+                padding: EdgeInsets.symmetric(horizontal: 14, vertical: 12),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color:
+                        SelectMethod == 'JazzCash'
+                            ? Colors.deepPurple
+                            : Colors.black26,
                   ),
-                    child: Row(
-                      children: [
-                        Icon(Icons.account_balance_wallet_rounded,color: Colors.deepPurple),
-                        SizedBox(width: 12),
-                         Expanded(child: Text("JazzCash",style: TextStyle(fontSize: 15),
-                         ),
-                         ),
-                           if(SelectMethod== 'JazzCash')
-                             Icon(Icons.check_circle,color: Colors.deepPurple),
-                      ],
-                    ),
-                  ),
+                  borderRadius: BorderRadius.circular(12),
+                  color: Colors.white,
                 ),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.account_balance_wallet_rounded,
+                      color: Colors.deepPurple,
+                    ),
+                    SizedBox(width: 12),
+                    Expanded(
+                      child: Text("JazzCash", style: TextStyle(fontSize: 15)),
+                    ),
+                    if (SelectMethod == 'JazzCash')
+                      Icon(Icons.check_circle, color: Colors.deepPurple),
+                  ],
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () => setState(() => SelectMethod = 'EasyPaisa'),
+              child: Container(
+                margin: EdgeInsets.only(bottom: 10),
+                padding: EdgeInsets.symmetric(horizontal: 14, vertical: 12),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color:
+                        SelectMethod == 'EasyPaisa'
+                            ? Colors.deepPurple
+                            : Colors.black26,
+                  ),
+                  borderRadius: BorderRadius.circular(12),
+                  color: Colors.white,
+                ),
+                child: Row(
+                  children: [
+                    Icon(Icons.phone_android, color: Colors.deepPurple),
+                    SizedBox(width: 12),
+                    Expanded(
+                      child: Text("EasyPaisa", style: TextStyle(fontSize: 15)),
+                    ),
+                    if (SelectMethod == 'EasyPaisa')
+                      Icon(Icons.check_circle, color: Colors.deepPurple),
+                  ],
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () => setState(() => SelectMethod = 'Bank Transfer'),
+              child: Container(
+                margin: EdgeInsets.only(bottom: 10),
+                padding: EdgeInsets.symmetric(horizontal: 14, vertical: 12),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color:
+                        SelectMethod == 'Bank Transfer'
+                            ? Colors.deepPurple
+                            : Colors.black26,
+                  ),
+                  borderRadius: BorderRadius.circular(12),
+                  color: Colors.white,
+                ),
+                child: Row(
+                  children: [
+                    Icon(Icons.account_balance, color: Colors.deepPurple),
+                    SizedBox(width: 12),
+                    Expanded(
+                      child: Text(
+                        "Bank Transfer",
+                        style: TextStyle(fontSize: 15),
+                      ),
+                    ),
+                    if (SelectMethod == 'Bank Transfer')
+                      Icon(Icons.check_circle, color: Colors.deepPurple),
+                  ],
+                ),
+              ),
+            ),
+               // Order Summery
+            Text("Order Summery",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),
+            ),
+               SizedBox(height: 10),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 16,vertical: 14),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                   child: Column(
+                     children: [
+                       SizedBox(height: 6),
+                       Row(
+                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                         children: [
+                           Text("Items Total",style: TextStyle(fontSize: 14),
+                           ),
+                              Text("Rs. 20000",style: TextStyle(fontSize: 14),
+                              ),
+                         ],
+                       ),
+                          SizedBox(height: 6),
+                         Row(
+                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                           children: [
+                             Text("Shipping",style: TextStyle(fontSize: 14),
+                             ),
+                              Text("Rs. 400",style: TextStyle(fontSize: 14),
+                              ),
+                           ],
+                         ),
+                       Divider(),
+                       Row(
+                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                         children: [
+                           Text("Total",style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),
+                           ),
+                           Text("Rs.12500",style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold,color: Colors.deepPurple),
+                           ),
+                         ],
+                       )
+                     ],
+                   ),
+              ),
+                SizedBox(height: 30),
           ],
         ),
       ),
